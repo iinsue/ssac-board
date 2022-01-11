f56512d4dac0fa50c4f86fd8b9ea6362b82117bb
iinsue <hinsu93@naver.com>
2022-01-11 10:50:05 +0900

    chapter19 공통오류페이지

---
+++ src/main/java/board/common/ExceptionHandler.java
@@ -0,0 +1,17 @@
+package board.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+	public ModelAndView defaultExceptionHandler(HttpServletRequest request, Exception exception) {
+		ModelAndView mv = new ModelAndView("/error/error_default");
+		mv.addObject("exception", exception);
+		return mv;
+	}
+}
