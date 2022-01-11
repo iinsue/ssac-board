1a38737c54507f48ba8e945c85c8bd54df3effde
iinsue <hinsu93@naver.com>
2022-01-11 10:46:29 +0900

    chapter18 트랜잭션

--- src/main/java/board/service/BoardServiceImpl.java
+++ src/main/java/board/service/BoardServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import board.dto.BoardDto;
 import board.mapper.BoardMapper;
 
+@Transactional
 @Service
 public class BoardServiceImpl implements BoardService {
 	
